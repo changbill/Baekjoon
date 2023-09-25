b1e87b64080942bf37515b341dbd4336b8f64827
changbill <128214736+changbill@users.noreply.github.com>
2023-09-26 00:10:02 +0900

    [Silver III] Title: N과 M (7), Time: 772 ms, Memory: 273680 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N,M;
+    static int NArr[], arr[];
+    static boolean visit[];
+    static StringBuilder sb = new StringBuilder();
+    public static void dfs(int depth) {
+        if(depth == M) {
+            for(int val: arr) {
+                sb.append(val+" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            arr[depth] = NArr[i];
+            dfs(depth+1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+        NArr = new int[N];
+        visit = new boolean[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            NArr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(NArr);
+        dfs(0);
+
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+}
