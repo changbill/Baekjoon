9957c5771d962bde71fd61ece24ce6e921e58c3e
changbill <128214736+changbill@users.noreply.github.com>
2024-03-08 23:14:49 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 528 ms, Memory: 95972 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -17,29 +17,25 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-        }       // 입력 완
+        }
 
-        valArr = new int[N];                // 가장 긴 증가하는 부분 수열 값을 넣을 배열
-        valArr[0] = arr[0];                 // 0번째 인덱스에는 arr[0] 넣기
-        int maxIdx = 0;                     // 현재 가장 큰 인덱스는 0임
+        valArr = new int[N];
+        valArr[0] = arr[0];
+        int maxIdx = 0;
         for (int i = 1; i < N; i++) {
-            if(arr[i] > valArr[maxIdx]) {       // arr의 해당 인덱스 값이 valArr의 가장 큰 값보다 크다면
-                valArr[++maxIdx] = arr[i];      // 새로운 인덱스에 해당 값 넣기
+            if(arr[i] > valArr[maxIdx]) {
+                valArr[++maxIdx] = arr[i];
             } else {
-                int valIdx = binarySearch(maxIdx, i);
-                valArr[valIdx] = arr[i];        // valArr 해당 인덱스 갈아치우기
-                // 갈아치우는 값은 그 이상의 값이지만 arr[i]값에 가장 가까운 값이다.
-                // 이 값은 이미 앞에서 구했던 배열에는 영향을 주지 않고 앞으로 구할 값들에만 영향을 주므로 문제되지 않는다
+                int valIdx = binarySearch(maxIdx, i); // 바꿀 valArr의 인덱스
+                valArr[valIdx] = arr[i];
             }
         }
         System.out.println(maxIdx+1);
     }
 
-    // 갈아치울 valArr의 인덱스 구하기
     public static int binarySearch(int maxIdx, int arrIdx) {
         int lo = 0;
         int hi = maxIdx;
-        // 우리가 구하려는 인덱스는 같은값 이상의 해당 값에 가장 가까운 값
         while(lo < hi) {
             int mid = (lo + hi)/2;
             if(valArr[mid] < arr[arrIdx]) {     // 찾을 값보다 작다면
