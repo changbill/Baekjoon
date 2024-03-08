3e007a4e4771635d54deb1a2e0466219c463d75b
changbill <128214736+changbill@users.noreply.github.com>
2024-03-08 21:40:09 +0900

    [Silver III] Title: N과 M (1), Time: 324 ms, Memory: 49832 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,47 +1,45 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.nio.Buffer;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N; // N까지 자연수 중에
-    static int M; // M개 뽑기
-    static boolean[] visit;
-    static int[] arr;
-
-    public static void dfs(int depth) {
-        if(depth == M) {
-            for(int val : arr) {
-                System.out.print(val + " ");
-            }
-            System.out.println();
-            return;
-        }
-
-        for (int i = 0; i < N; i++) {
-            if (!visit[i]) {
-                visit[i] = true;
-                arr[depth] = i+1;
-                dfs(depth+1);
-                visit[i] = false;
-            }
-        }
-    }
-
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        visit = new boolean[N];
-        arr = new int[M];
-
-        dfs(0);
-
-    }
+	
+	static int N, M, arr[];
+	static boolean visited[];
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		arr = new int[M];
+		visited = new boolean[N];
+		
+		dfs(0);
+		
+		System.out.println(sb.toString());
+	}
+	
+	static void dfs(int depth) {
+		if(depth == M) {
+			for(int i = 0; i< M; i++) {
+				sb.append(arr[i]+" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i = 1; i<= N; i++) {
+			if(!visited[i-1]) {
+			arr[depth]= i;
+			visited[i-1] = true;
+			dfs(depth+1);
+			visited[i-1] = false;
+			}
+		}
+		
+	}
 }
