01fbf7e77962de9d1360c5e50555f7976e902424
changbill <128214736+changbill@users.noreply.github.com>
2024-08-28 18:01:14 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 100 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int dp[] = new int[1001];
+        dp[1] = 1;
+        dp[2] = 3;
+        for(int i = 3; i <= 1000; i++) {
+            dp[i] = (dp[i-1] + 2*dp[i-2])%10007;
+        }
+
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(dp[N]);
+    }
+}
