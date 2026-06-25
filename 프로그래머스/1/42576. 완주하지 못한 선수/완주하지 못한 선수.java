73feb770ece16fb6a4e773de7c0ed27a0e0098c9
이창헌 <128214736+changbill@users.noreply.github.com>
2026-06-25 17:37:00 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 55.72 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,23 +2,47 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Arrays.sort(participant);
-        Arrays.sort(completion);
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String comp : completion) {
+            map.put(comp, map.getOrDefault(comp, 0) + 1);
+        }
         
-        int pmax = participant.length;
-        int cmax = completion.length;
-        int pidx = 0;
-        int cidx = 0;
-        
-        while(pidx < pmax && cidx < cmax) {
-            if(participant[pidx].equals(completion[cidx])) {
-                pidx++;
-                cidx++;
-            } else {
-                return participant[pidx];
+        for(String part : participant) {
+            if(!map.containsKey(part)) {
+                return part;
+            }
+            map.put(part, map.get(part) - 1);
+            if(map.get(part) < 0) {
+                return part;
             }
         }
         
-        return participant[pidx];
+        return "";
     }
 }
+
+
+// import java.util.*;
+
+// class Solution {
+//     public String solution(String[] participant, String[] completion) {
+//         Arrays.sort(participant);
+//         Arrays.sort(completion);
+        
+//         int pmax = participant.length;
+//         int cmax = completion.length;
+//         int pidx = 0;
+//         int cidx = 0;
+        
+//         while(pidx < pmax && cidx < cmax) {
+//             if(participant[pidx].equals(completion[cidx])) {
+//                 pidx++;
+//                 cidx++;
+//             } else {
+//                 return participant[pidx];
+//             }
+//         }
+        
+//         return participant[pidx];
+//     }
+// }
