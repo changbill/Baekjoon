ec5816a7f518824e15e0f9274da1b299dcd70d08
이창헌 <128214736+changbill@users.noreply.github.com>
2026-06-25 18:04:34 +0900

    [level 2] Title: 전화번호 목록, Time: 184.79 ms, Memory: 239 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,22 +2,51 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book);
-        String prev = "s";
-        loop:
+        Set<String> set = new HashSet<>();
         for(String str : phone_book) {
-            if(prev.length() <= str.length()) {
-                for(int i = 0; i < prev.length(); i++) {
-                    if(prev.charAt(i) != str.charAt(i)) {
-                        prev = str;
-                        continue loop;
-                    }
-                }
-                return false;
-            }
-            prev = str;
+            set.add(str);
         }
-        
+        for(String str : phone_book) {
+            for(int i = 1; i < str.length(); i++) {
+                String tmp = str.substring(0,i);
+                if(set.contains(tmp)) {
+                    return false;
+                }
+            }
+        }
         return true;
     }
 }
+
+
+
+
+
+
+
+
+
+
+// import java.util.*;
+
+// class Solution {
+//     public boolean solution(String[] phone_book) {
+//         Arrays.sort(phone_book);
+//         String prev = "s";
+//         loop:
+//         for(String str : phone_book) {
+//             if(prev.length() <= str.length()) {
+//                 for(int i = 0; i < prev.length(); i++) {
+//                     if(prev.charAt(i) != str.charAt(i)) {
+//                         prev = str;
+//                         continue loop;
+//                     }
+//                 }
+//                 return false;
+//             }
+//             prev = str;
+//         }
+        
+//         return true;
+//     }
+// }
