214e8403d8338fb73d203c4edab9580478455098
이창헌 <128214736+changbill@users.noreply.github.com>
2026-06-25 17:45:07 +0900

    [level 1] Title: 폰켓몬, Time: 2.35 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,18 +2,43 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
+        int n = nums.length;
         Set<Integer> set = new HashSet<>();
-        for(int i = 0; i < nums.length; i++) {
+        for(int i = 0 ; i < n; i++) {
             set.add(nums[i]);
         }
         
-        int answer = 0;
-        if(nums.length / 2 >= set.size()) {
-            answer = set.size();
-        } else {
-            answer = nums.length / 2;
-        }
+        if(set.size() > n/2) return n/2;
         
-        return answer;
+        return set.size();
     }
 }
+
+
+
+
+
+
+
+
+
+
+// import java.util.*;
+
+// class Solution {
+//     public int solution(int[] nums) {
+//         Set<Integer> set = new HashSet<>();
+//         for(int i = 0; i < nums.length; i++) {
+//             set.add(nums[i]);
+//         }
+        
+//         int answer = 0;
+//         if(nums.length / 2 >= set.size()) {
+//             answer = set.size();
+//         } else {
+//             answer = nums.length / 2;
+//         }
+        
+//         return answer;
+//     }
+// }
