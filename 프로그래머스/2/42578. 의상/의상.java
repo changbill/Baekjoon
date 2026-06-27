426c83e596284a831991bc33ec8b38365bfd279a
이창헌 <128214736+changbill@users.noreply.github.com>
2026-06-27 14:01:55 +0900

    [level 2] Title: 의상, Time: 0.09 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,20 +2,59 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        HashMap<String, Integer> map = new HashMap<>();
-        
-        for (String[] cloth : clothes) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        for(String[] cloth : clothes) {
+            String name = cloth[0];
             String type = cloth[1];
-            map.put(type, map.getOrDefault(type, 0) + 1);
+            
+            List<String> list;
+            if(map.containsKey(type)) {
+                list = map.get(type);
+            } else {
+                list = new ArrayList<>();
+                map.put(type, list);
+            }
+            list.add(name);
         }
         
         int answer = 1;
-        Iterator<Integer> it = map.values().iterator();
-        
-        while (it.hasNext()) {
-            answer *= (it.next() + 1);
+        for(Map.Entry<String, List<String>> entry : map.entrySet()) {
+            List<String> list = entry.getValue();
+            int len = list.size();
+            answer *= len+1;
         }
         
-        return answer - 1;
+        return answer -1;
     }
 }
+
+
+
+
+
+
+
+
+
+
+// import java.util.*;
+
+// class Solution {
+//     public int solution(String[][] clothes) {
+//         HashMap<String, Integer> map = new HashMap<>();
+        
+//         for (String[] cloth : clothes) {
+//             String type = cloth[1];
+//             map.put(type, map.getOrDefault(type, 0) + 1);
+//         }
+        
+//         int answer = 1;
+//         Iterator<Integer> it = map.values().iterator();
+        
+//         while (it.hasNext()) {
+//             answer *= (it.next() + 1);
+//         }
+        
+//         return answer - 1;
+//     }
+// }
